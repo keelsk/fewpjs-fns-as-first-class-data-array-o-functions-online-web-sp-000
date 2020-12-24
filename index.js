581baf0da7dbe85094ca61dbaf1632ff1c6458ab
Kirsten Keels <keelsk@gmail.com>
2020-12-24 02:14:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ function wakeDog(dogName, dogBreed) {
 }
 
 function leashDog(dogName, dogBreed) {
-  console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed}`;
 }
 
 function walkToPark(dogName, dogBreed) {
-  console.log(`Wake to the park with ${dogName} the ${dogBreed}`);
+  return `Wake to the park with ${dogName} the ${dogBreed}`;
 }
 
 function throwFrisbee(dogName, dogBreed) {
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`;
 }
 
 function walkHome(dogName, dogBreed) {
-  console.log(`Wake home with ${dogName} the ${dogBreed}`);
+  return `Wake home with ${dogName} the ${dogBreed}`;
 }
 
 function unleashDog(dogName, dogBreed) {
-  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `Unleash ${dogName} the ${dogBreed}`;
 }
 
 let routine = [wakeDog,leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
